4cfd7ba5f37875a2b2a40c86cce9613e1e687299
michael williams <manicdesigner@gmail.com>
2018-10-31 17:36:57 -0400

    add header info for include

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikey
+ * Date: 10/31/18
+ * Time: 4:34 PM
+ */
+
+$page_title = 'Med App';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php $page_title ?></title>
+</head>
+
