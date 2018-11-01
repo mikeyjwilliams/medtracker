3fad2a5cd1601f0f3d0551245c742faa1cb6ff43
michael williams <manicdesigner@gmail.com>
2018-11-01 10:48:06 -0400

    created file for template

---
+++ patients.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikey
+ * Date: 11/1/18
+ * Time: 9:58 AM
+ */
+?>
+
+<?php require_once 'inc/header.php'; ?>
+<?php require_once 'inc/nav.php'; ?>
+
+
+
+
+
+
+<?require_once 'inc/footer.php'; ?>
