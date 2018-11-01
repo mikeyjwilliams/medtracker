ed9bac4b06c1c6dfb0ae66f8633055d2673ed2ca
michael williams <manicdesigner@gmail.com>
2018-10-31 22:36:35 -0400

    cp dropdown 'Add Items'

--- inc/nav.php
+++ inc/nav.php
@@ -23,7 +23,7 @@
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarMedDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                    Add Items
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarMedDropdown">
                     <a class="dropdown-item" href="#">Add Patient</a>
