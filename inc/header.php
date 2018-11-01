5c1c260af4c708ba77f86301edf6c39fef8c2f5d
michael williams <manicdesigner@gmail.com>
2018-11-01 01:16:59 -0400

    add fontawesome needed style sheet.

--- inc/header.php
+++ inc/header.php
@@ -19,6 +19,7 @@ $page_title = 'Med App';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/fontawesome.min.css">
+    <link rel="stylesheet" href="/css/all.min.css">
     <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" href="/css/main.css">
 </head>
