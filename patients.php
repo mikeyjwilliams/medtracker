24b2b329686bff4b8b98938b6b7e5c77a67eda0e
michael williams <manicdesigner@gmail.com>
2018-11-01 11:11:31 -0400

    created the template for the patients file.

--- patients.php
+++ patients.php
@@ -10,9 +10,32 @@
 <?php require_once 'inc/header.php'; ?>
 <?php require_once 'inc/nav.php'; ?>
 
+<form action="/patient_list.php" method="POST">
+    <div class="container my-4 my-1">
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <div class="form-group">
+                    <label for="patientFirstName">Patient First Name</label>
+                    <input type="text" class="form-control" name="patientFirstName" id="patientFirstName" placeholder="John" required>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <div class="form-group">
+                    <label for="patientLastName">Patient Last Name</label>
+                    <input type="text" class="form-control" name="patientLastName" id="patientLastName" placeholder="Smith" required>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6 col-md-6">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </div>
+
+</form>
 
 
 
 
-
-<?require_once 'inc/footer.php'; ?>
+<?php include 'inc/footer.php'; ?>
