4af9212d1bcaa4898a8fa161b0992747da770ba4
michael williams <manicdesigner@gmail.com>
2018-10-31 22:11:00 -0400

    fixing link file structures.

--- inc/header.php
+++ inc/header.php
@@ -18,7 +18,10 @@ $page_title = 'Med App';
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/fontawesome.min.css">
     <title><?php $page_title ?></title>
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="/css/main.css">
 </head>
+<html>
+
 
