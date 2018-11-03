d340eaee31ad938d991a20dd2df4f9fc14cd595d
michael williams <manicdesigner@gmail.com>
2018-11-03 16:10:32 -0400

    add add_name function to add first and last name report_name give first and last name out.

--- inc/functions.php
+++ inc/functions.php
@@ -5,3 +5,37 @@
  * Date: 10/31/18
  * Time: 11:36 PM
  */
+
+
+/** @param $pat_first_name
+ * @param $pat_last_name
+ * @return bool
+ */
+function add_name($pat_first_name, $pat_last_name) {
+    include 'conn.php';
+    $sql = "INSERT INTO Patients(pat_first_name, pat_last_name) 
+              VALUES(?, ?)";
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1,$pat_first_name, PDO::PARAM_STR);
+        $results->bindValue(2, $pat_last_name, PDO::PARAM_STR);
+        $results->execute();
+    } catch(Exception $e) {
+        echo 'Error! ' . $e->getMessage() . "<br>";
+        return false;
+    }
+    return true;
+}
+
+function report_name()
+{
+    include 'conn.php';
+
+    try {
+        return $db->query('SELECT pat_first_name, pat_last_name FROM Patients');
+
+    } catch (Exception $e) {
+        echo 'Error! ' . $e->getMessage() . '<br>';
+        return array();
+    }
+}
