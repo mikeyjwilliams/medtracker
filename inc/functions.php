d48c7dbf4ecf1d7e2d913f5c486392a182f52924
michael williams <manicdesigner@gmail.com>
2018-11-03 22:09:08 -0400

    add add_doc -> insert doctor into db. report_docs -> show doctors

--- inc/functions.php
+++ inc/functions.php
@@ -11,14 +11,31 @@
  * @param $pat_last_name
  * @return bool
  */
-function add_name($pat_first_name, $pat_last_name) {
+function add_name($pat_first_name, $pat_last_name)
+{
     include 'conn.php';
     $sql = "INSERT INTO Patients(pat_first_name, pat_last_name) 
               VALUES(?, ?)";
     try {
         $results = $db->prepare($sql);
-        $results->bindValue(1,$pat_first_name, PDO::PARAM_STR);
+        $results->bindValue(1, $pat_first_name, PDO::PARAM_STR);
         $results->bindValue(2, $pat_last_name, PDO::PARAM_STR);
+        $results->execute();
+    } catch (Exception $e) {
+        echo 'Error! ' . $e->getMessage() . "<br>";
+        return false;
+    }
+    return true;
+}
+
+function add_doc($doc_first_name, $doc_last_name) {
+    include 'conn.php';
+    $sql = "INSERT INTO Doctors(dr_first_name, dr_last_name) 
+              VALUES(?, ?)";
+    try {
+        $results = $db->prepare($sql);
+        $results->bindValue(1,$doc_first_name, PDO::PARAM_STR);
+        $results->bindValue(2, $doc_last_name, PDO::PARAM_STR);
         $results->execute();
     } catch(Exception $e) {
         echo 'Error! ' . $e->getMessage() . "<br>";
@@ -27,12 +44,25 @@ function add_name($pat_first_name, $pat_last_name) {
     return true;
 }
 
-function report_name()
+function report_names()
 {
     include 'conn.php';
 
     try {
         return $db->query('SELECT pat_first_name, pat_last_name FROM Patients');
+
+    } catch (Exception $e) {
+        echo 'Error! ' . $e->getMessage() . '<br>';
+        return array();
+    }
+}
+
+function report_docs()
+{
+    include 'conn.php';
+
+    try {
+        return $db->query('SELECT dr_first_name, dr_last_name FROM Doctors');
 
     } catch (Exception $e) {
         echo 'Error! ' . $e->getMessage() . '<br>';
