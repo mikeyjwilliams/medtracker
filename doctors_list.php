293aa3e6268facae38417484ec5dca1d4c954506
michael williams <manicdesigner@gmail.com>
2018-11-03 22:26:20 -0400

    started doctors_list header

---
+++ doctors_list.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikey
+ * Date: 11/3/18
+ * Time: 10:14 PM
+ */
+require 'inc/functions.php';
+
+?>
+
+
+<?php require_once 'inc/header.php'; ?>
+<?php require_once 'inc/nav.php'; ?>
+
+<div class="container my-1 my-md-5">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1 class="text-center">Doctors List</h1>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+
+        </div>
+    </div>
+</div>
