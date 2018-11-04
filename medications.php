829b779bd507a7d9b93d78f4c66cc39b765ce431
michael williams <manicdesigner@gmail.com>
2018-11-04 17:39:54 -0500

    add filter_inputs to file

--- medications.php
+++ medications.php
@@ -6,6 +6,16 @@
  * Time: 12:24 AM
  */
 
+if($_SERVER['REQUEST_METHOD'] === 'post') {
+    $med_name = trim(filter_input(INPUT_POST, 'med_name', FILTER_SANITIZE_STRING));
+    $med_rx = trim(filter_input(INPUT_POST, 'med_rx', FILTER_SANITIZE_STRING));
+    $med_quantity = trim(filter_input(INPUT_POST, 'med_quantity', FILTER_SANITIZE_NUMBER_INT));
+
+}
+
+$page_title = 'Meds Form | Med App';
+$page = 'meds_form';
+
 include 'inc/functions.php';
 
 ?>
@@ -70,8 +80,7 @@ include 'inc/functions.php';
            <button type="submit" class="btn btn-primary">Submit</button>
        </div>
 
-
-
-
    </div>
 </form>
+
+<?php include './inc/footer.php'; ?>
