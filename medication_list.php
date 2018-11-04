84ba2258fc3ce0bc866c9ae0a0707c3a2180a4c2
michael williams <manicdesigner@gmail.com>
2018-11-04 01:42:59 -0400

    test dummy sheet

--- medication_list.php
+++ medication_list.php
@@ -5,3 +5,18 @@
  * Date: 11/4/18
  * Time: 1:15 AM
  */
+
+$med_name = $_POST['med_name'];
+$med_rx = $_POST['med_rx'];
+$med_quantity = $_POST['med_quantity'];
+$fill_date = $_POST['fill_date'];
+$med_per_dose = $_POST['med_per_dose'];
+$dr_id = $_POST['dr_id'];
+$patient_id = $_POST['patient_id'];
+
+echo 'medication name is ' . $med_name . "<br>";
+echo 'med rx is ' . $med_rx . "<br>";
+echo 'med quantity is ' . $med_quantity . "<br>";
+echo 'fill date is ' . $fill_date . "<br>";
+echo 'dr id is ' . $dr_id . "<br>";
+echo 'patient id is ' . $patient_id . "<br>";
