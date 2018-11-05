5af8b2f4595fa1e6bba614d938ee72a72443c475
michael williams <manicdesigner@gmail.com>
2018-11-05 01:41:40 -0500

    beginning of page.

--- reports.php
+++ reports.php
@@ -5,3 +5,49 @@
  * Date: 10/31/18
  * Time: 11:36 PM
  */
+require_once './inc/strap.php';
+
+?>
+
+<?php include './inc/header.php'; ?>
+<?php include './inc/nav.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <h1>Reports</h1>
+        </div>
+    </div>
+</div>
+<form action="reports.php" method="POST">
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="group-select">Options</label>
+                </div>
+                <select class="custom-select" id="group-select">
+                    <option selected>Choose...</option>
+                    <option value="">Full Report</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <button type="submit" class="btn btn-primary">Show Report</button>
+        </div>
+    </div>
+</div>
+</form>
+<?php
+if(isset($report)) { ?>
+    <div class="container">
+        <div class="row">
+
+        </div>
+    </div>
+<?php }  ?>
+
+
