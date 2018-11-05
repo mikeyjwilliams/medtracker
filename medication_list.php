83f772db9e0865cc6eb461625afb6f3264ec5226
mikey <manicdeisgner@gmail.com>
2018-11-04 21:09:43 -0500

    filter_input for all posts needed for add_medication func.

--- medication_list.php
+++ medication_list.php
@@ -6,17 +6,26 @@
  * Time: 1:15 AM
  */
 
-$med_name = $_POST['med_name'];
-$med_rx = $_POST['med_rx'];
-$med_quantity = $_POST['med_quantity'];
-$fill_date = $_POST['fill_date'];
-$med_per_dose = $_POST['med_per_dose'];
-$dr_id = $_POST['dr_id'];
-$patient_id = $_POST['patient_id'];
+//$med_name = $_POST['med_name'];
+//$med_rx = $_POST['med_rx'];
+//$med_quantity = $_POST['med_quantity'];
+//$fill_date = $_POST['fill_date'];
+//$med_per_dose = $_POST['med_per_dose'];
+//$dr_id = $_POST['dr_id'];
+//$patient_id = $_POST['patient_id'];
+
+$med_name = trim(filter_input(INPUT_POST, 'med_name', FILTER_SANITIZE_STRING));
+$med_rx = trim(filter_input(INPUT_POST, 'med_rx', FILTER_SANITIZE_STRING));
+$med_quantity = trim(filter_input(INPUT_POST, 'med_quantity', FILTER_SANITIZE_NUMBER_INT));
+$med_date = trim(filter_input(INPUT_POST, 'fill_date', FILTER_SANITIZE_STRING));
+$med_per_dose = trim(filter_input(INPUT_POST, 'med_per_dose', FILTER_SANITIZE_NUMBER_INT));
+$dr_id = trim(filter_input(INPUT_POST, 'dr_id', FILTER_SANITIZE_NUMBER_INT));
+$patient_id = trim(filter_input(INPUT_POST, 'patient_id', FILTER_SANITIZE_NUMBER_INT));
 
 echo 'medication name is ' . $med_name . "<br>";
 echo 'med rx is ' . $med_rx . "<br>";
 echo 'med quantity is ' . $med_quantity . "<br>";
-echo 'fill date is ' . $fill_date . "<br>";
+echo 'fill date is ' . $med_date . "<br>";
+echo 'per dosage ' . $med_per_dose . "<br>";
 echo 'dr id is ' . $dr_id . "<br>";
 echo 'patient id is ' . $patient_id . "<br>";
