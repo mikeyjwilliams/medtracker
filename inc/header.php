7d15b00245c9d9d8077bc3e662474064d4d6f2cb
mikey <manicdesigner@gmail.com>
2019-03-06 09:45:13 -0500

    upgraded to bootstrap 4.3.1 freezing to 4.3.1 now.

--- inc/header.php
+++ inc/header.php
@@ -16,8 +16,8 @@ if(!isset($page_title)) { echo 'Med App'; }
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/fontawesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+   <link rel="stylesheet" href="/css/fontawesome.min.css">
     <link rel="stylesheet" href="/css/all.min.css">
     <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" href="/css/main.css">
